e3cd02a4e86106be356e23e74bac7d4a8ceeb3f5
GerryCrooked <pinnhead1987@googlemail.com>
2021-12-17 10:46:05 +0100

    Add files via upload

---
+++ install_jabra.ps1
@@ -0,0 +1,5 @@
+﻿import-module BitsTransfer
+
+Start-BitsTransfer https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe C:\Temp\jabra.msi
+Set-Content c:\temp\install_jabra.bat 'c:\temp\jabra.msi /quiet' -Encoding Ascii
+start-process c:\temp\install_jabra.bat
